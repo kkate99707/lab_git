7f1175abf301e179312d49c08dbbfe17e0fa4486
Kot <kkate99707@gmail.com>
2025-01-24 14:48:32 +0300

    изменение result

---
+++ src/utils.py
@@ -0,0 +1,8 @@
+def add_numbers(x, y):
+    result = x + y
+    print(f"Сумма: {result}")
+    return result
+def multiply_numbers(x, y):
+    result = x * y
+    print(f"Произведение: {result}")
+    return result
